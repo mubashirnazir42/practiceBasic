816b569d2fe94ce89429a28a7c9e2d411dd3102d
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2023-12-24 12:26:50 +0500

    ms

---
+++ script.js
@@ -0,0 +1,10 @@
+const friends = ['zeeshan' , 'hamza', 'ehsaan'];
+
+console.log(friends[friends.length-3]);
+console.log(friends);
+
+const myDate =['mubashir',age=24,'tan color', 'agressive',23];
+console.log(myDate[1]);
+
+
+console.log(myDate.includes('tan color'));
