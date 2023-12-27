4dd680c28423b253eee57b036ccc9eb67cde6c0f
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2023-12-28 02:15:18 +0500

    committed

--- script.js
+++ script.js
@@ -1,99 +1,29 @@
+/* Write a function calcAverage which takes an array called arr as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it if you feel like it:
+
+First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+Call the function with the totals array.
+
+*/
+
 "use strict"
 
-const birthYear = [1991,  "ali", 1986 , 2012, 2000]; 
-const ages= []; 
-for(let i=0 ; i<=birthYear.length-1 ; i++){ 
-  if(typeof birthYear[i] !== 'number') continue;
-  console.log(typeof birthYear[i]);
-      ages.push(2024 - birthYear[i]) 
-
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
-console.log(ages);
+const bills= [22, 295, 176, 440, 37, 105, 10, 1100, 86,  52];
+const tips =[];
+const totals =[];
 
-const boyfriendIsPaying = false;
-const isSale = false;
 
-if (boyfriendIsPaying || !isSale) {
-  console.log("Jesse will go shopping.");
-} else {
-  console.log("Jesse will not go shopping.");
+
+for(let i=0 ; i<= bills.length-1 ;i++){
+     tips[i]=calcTip(bills[i]) 
+     totals.push(bills[i] + tips[i] )   
 }
 
-const age = 12;
-if(age !==24){
-console.log(`can apply in uni`);
-}
-
-for(let k=0; k<=4 ; k++){
-  if(typeof(age) !== 'string') break;
-    console.log(`done`)
-    
-}
-
-for(let k=0; k<=10 ; k++){
-if(k==2) continue;
-console.log(`${k}`)
-}
-
-
-
-let friend = [
-
-  'mubashir',
-  [23,45,23],
-  2024-1994,
-  [`waseem`,'john','jonas']
-
-]
-const x=[]
-
-for(let i=friend.length-1;i>=0 ; i--){
-  
- x.push(friend[i]);
-}
-
-console.log(x);
-
-
-for(let i=1 ; i<=3;i++){
-  for(let j=1 ; j<=3 ; j++){
-    for(k=1; k <=5 ; k++){
-      console.log(`${i}, ${j} , ${k}  `);
-    }
-  }
-}
-
-
-for(let j=3 ; j>=1;j--){
-  for(let k=5 ; k>=3 ; k--){
-    console.log(`${j} , ${k} `);
-  }
-}
-
-for(let x=1;x<=3;x++){
-  console.log(`-----------${x}-----------------`);
-  for(let k=4 ; k<=7 ; k++){
-    console.log(`-----------${k}-----------------`);
-    for(let  z=5; z>=1 ; z-- ){
-      console.log(`-----------${z}-----------------`);
-    }
-  }
-}
-
-let v=1;
-
-while(v<=10){
-  console.log(`${v}`);
-  v++;
-}
-
-let randomNumber= Math.trunc(Math.random()*6)+1;
-console.log(randomNumber);
-
-while(randomNumber !==6 ){
-  console.log(` you rolled a dice ${randomNumber}`);
-  randomNumber= Math.trunc(Math.random()*6)+1;
-  console.log(randomNumber);
-  
-}
+console.log(tips);
+console.log(totals);
