de2251acb3ce5cc658f59853b10506652fc4e718
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2023-12-28 03:15:52 +0500

    sky

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-/* Write a function calcAverage which takes an array called arr as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it if you feel like it:
+/* Write a function calcAverage which takes an array called arr as an argument. This function calculates
+ the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)!
+  Here is how to solve it if you feel like it:
 
-First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+First, you will need to add up all values in the array. To do the addition, start by creating 
+a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration,
+ add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
 
-To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+To calculate the average, divide the sum you calculated before by the length of the 
+array (because that's the number of elements).
 
 Call the function with the totals array.
 
 */
 
 "use strict"
+
+
 
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
@@ -27,3 +34,17 @@ for(let i=0 ; i<= bills.length-1 ;i++){
 
 console.log(tips);
 console.log(totals);
+
+const calcAverage = function(arr){
+  let sum=0
+  for(let i=0; i<= arr.length-1; i++){
+    sum= sum + arr[i]
+    
+  }
+const avg= sum/ arr.length;
+return avg;
+}
+
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+console.log(calcAverage([2,3,7]));
