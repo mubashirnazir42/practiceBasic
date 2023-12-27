f2b5c2ccc33aa7aab078a61b09e269c36b27e8dc
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2023-12-27 17:30:02 +0500

    bsic

--- script.js
+++ script.js
@@ -1,68 +1,57 @@
-for(let k=0; k<=2;k++){
-  for(let i=1; i<=10; i=i+1){
-    console.log(`2 * ${i} = ${2*i} `);
+const subYear = function(saal){
+return saal/2;
+}
+
+const birthYear = function(year){
+
+
+  for(i=0; i <=year.length-1;i++){
+           
+    const x =subYear(year[i]);
+    console.log(x);
+    
+
+    console.log(`my age is ${2024- year[i]}`); 
+  }
+
+
+}
+birthYear([1994,1997,2000,2010]);
+
+
+
+
+
+
+for(let rep =5; rep <= 10 ; rep= rep+1){
+  
+  console.log(`lifting up weight ${rep} 🏆 `);
   
   
+}
+
+const aliRaza =[
+  `ali`,
+  "raza",
+  2024-1994,
+  "sheikh",
+  [21,45,67],
+  {
+    fName : 'waqas',
+    sName : 'raheem',
+    tName : 'jonas'
   }
-  console.log(`hi ${k}`);
- 
+];
+
+const type=[];
+
+for(i=0 ; i<=aliRaza.length-1;i++){
+  console.log(aliRaza[i], typeof(aliRaza[i]));
+// type.push(typeof aliRaza[i]);
+type[i] = aliRaza[i]
 }
 
-console.log(`=====>the end<======`);
+console.log(type);
 
-const movies = [ `alexander` , `wall of karachi` , 'anonymousfunction' ];
-
-for(let l = 0; l<=movies.length-1 ; l++){
- console.log( movies[l]);
-}
-
-movies.push('hi');
-console.log(movies);
-
-
-
-
-const birthYear = [1998 , 1994 , 2000 , 1876];
-
-
-for(let i=0 ; i<= birthYear.length-1 ; i++){
-
-const age = 2024 - birthYear[i]
-console.log(` my age is ${age} `);
-
-}
-
-for(let breakPoint = 5 ; breakPoint>=0 ; breakPoint--){
-console.log(birthYear[breakPoint]);
-}
-
-
-
-for(i=0; i<=2 ; i++){
-
-  console.log(`${i}`);
-
-  for(j=i+1 ; j<=5 ; j++){
-    console.log(`${j}`);
-  }
-   
-}
-
-
-
-for(i=10 ; i>=3 ; i--){
-console.log(`outerloop`);  //10                 ,9,                 8,                7
-  for(j=1;j<=5 ; j++){
-    console.log(`innerloop`); // 1,2,3,4,5,      1,2,3,4,5          1,2,3,4,5         1,2,3,4,5
-  }
-
-}
-
-const y=Math.trunc(Math.random()*5)
-console.log(`${y}`);
-while(y==3){
-  console.log(`stop ${y}`);
-  break;
-}
 
 
